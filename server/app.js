07fc410769b4dc9eeb0b5ca1b681037e830a0f9f
Evan Kolb <kongaiwen@gmail.com>
2020-09-05 15:22:46 -0700

    add k6 file to repo

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const express = require('express');
-
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/../public')));
+app.use('/games/:gameId', express.static(path.join(__dirname, '/../public')));
+
+//app.use('/api/gameById/:gameId', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+//app.use('/api/bundlesByGameId/:gameId', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+
+
 
 module.exports.app = app;
